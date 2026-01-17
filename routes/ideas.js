04e739384b2e1d3bbeba5c022f55dd1915d05321
MeshachB <MeshachCBrown@gmail.com>
2026-01-16 20:16:08 -0500

    Complete Ideas CRUD with RESTful routes and EJS views

--- routes/ideas.js
+++ routes/ideas.js
@@ -18,6 +18,43 @@ router.get('/new', (req, res) => {
   res.render('ideas/new');
 });
 
+router.post('/', async (req, res) => {
+  try {
+    await Idea.create(req.body);
+    res.redirect('/ideas');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.redirect('/ideas');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.render('ideas/edit', { idea });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+router.put('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect('/ideas');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 
 
 
